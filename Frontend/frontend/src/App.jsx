de8fa4a4b9668e5a147e4166dec134d21c45f407
Sontosh37271 <sontoshshil37271@gmail.com>
2025-01-30 18:52:43 +0600

    I write all frontend code here

---
+++ Frontend/frontend/src/App.jsx
@@ -0,0 +1,17 @@
+
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      1.Login
+      2.Signup
+      3.authentication
+      4.other features
+    </>
+  )
+}
+
+export default App
